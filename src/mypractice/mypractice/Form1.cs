c6493f85703dc40894f5f74f69ac8b5ad8dec857
valensys <keep.this.street.empty.for.me@gmail.com>
2018-03-09 00:25:10 +0300

    practice

--- src/mypractice/mypractice/Form1.cs
+++ src/mypractice/mypractice/Form1.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -12,9 +12,59 @@ namespace mypractice
 {
 	public partial class Form1 : Form
 	{
+		private static int capacity = 100;
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+		{
+			capacity = (int)numericUpDown1.Value;
+		}
+
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+			listBox1.Items.Clear();
+			ArrayList numbers = new ArrayList(capacity);
+			for (int i=1; i<=numbers.Capacity; i++)
+			listBox1.Items.Add(i);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			listBox2.Items.Clear();
+			for (int i=1; i<listBox1.Items.Count; i++)
+			{
+				if((i > 5) && ((int)listBox1.Items[i] > 10) && ((int)listBox1.Items[i] < 100) && (listBox2.Items.Count < 20))
+					listBox2.Items.Add(listBox1.Items[i]);
+
+			}
 		}
 	}
 }
